d6b3456c90cc6ac6bfd2109855360721c86c45b5
Andrew Couch <andy@couchand.com>
2013-05-05 13:19:57 -0400

    class_references view

---
+++ dashboard/views/class_references/map.js
@@ -0,0 +1,16 @@
+function (doc) {
+  if ( !doc.classes[0] || !doc.classes[0].SymbolTable ) {
+    return;
+  }
+  for ( i in doc.classes ) {
+    cls = doc.classes[i];
+    for ( j in cls.SymbolTable.externalReferences ) {
+      ref = cls.SymbolTable.externalReferences[j];
+      emit(ref.name + ":" + cls.SymbolTable.name, {
+        from: cls.SymbolTable.name
+        to: ref.name
+        count: ref.references.length
+      });
+    }
+  }
+}
